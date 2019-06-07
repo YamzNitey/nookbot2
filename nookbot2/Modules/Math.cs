25dcbdbfe357858580ee6ace3fd6febc6d933fb7
Yamz9983 <41996306+Yamz9983@users.noreply.github.com>
2019-06-07 17:46:32 +0100

    bitwise operators

--- nookbot2/Modules/Math.cs
+++ nookbot2/Modules/Math.cs
@@ -11,38 +11,68 @@ namespace nookbot2.Modules
 {
     public class Math : ModuleBase<SocketCommandContext>
     {
-        [Command("add")]
+        [Command("add", true)]
         public async Task Add(double num1, double num2)
         {
             await ReplyAsync($"Sum: `{num1}` + `{num2}` = `{num1 + num2}`");
         }
 
-        [Command("subtract")]
+        [Command("subtract", true)]
         [Alias("sub")]
         public async Task Subtract(double num1, double num2)
         {
             await ReplyAsync($"Sum: `{num1}` - `{num2}` = `{num1 - num2}`");
         }
 
-        [Command("multiply")]
+        [Command("multiply", true)]
         [Alias("mul")]
         public async Task Multiply(double num1, double num2)
         {
             await ReplyAsync($"Sum: `{num1}` * `{num2}` = `{num1 * num2}`");
         }
 
-        [Command("divide")]
+        [Command("divide", true)]
         [Alias("div")]
         public async Task Divide(double num1, double num2)
         {
             await ReplyAsync($"Sum: `{num1}` / `{num2}` = `{num1 / num2}`");
         }
 
-        [Command("modulo")]
+        [Command("modulo", true)]
         [Alias("mod")]
         public async Task Modulo(double num1, double num2)
         {
             await ReplyAsync($"Sum: `{num1}` % `{num2}` = `{num1 % num2}`");
+        }
+
+        [Command("and", true)]
+        public async Task And(long num1, long num2)
+        {
+            await ReplyAsync($"Result: `{num1}` & `{num2}` = `{num1 & num2}`");
+        }
+
+        [Command("or", true)]
+        public async Task Or(long num1, long num2)
+        {
+            await ReplyAsync($"Result: `{num1}` | `{num2}` = `{num1 | num2}`");
+        }
+
+        [Command("xor", true)]
+        public async Task Xor(long num1, long num2)
+        {
+            await ReplyAsync($"Result: `{num1}` ^ `{num2}` = `{num1 ^ num2}`");
+        }
+
+        [Command("not", true)]
+        public async Task Not(long num)
+        {
+            await ReplyAsync($"Result: ~`{num}` = `{~num}`");
+        }
+
+        [Command("tc", true)]
+        public async Task TwosComplement(long num)
+        {
+            await ReplyAsync($"Result: ~`{num}` + 1 = `{~num + 1}`");
         }
     }
 }
